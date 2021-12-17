28f706f4d4326eb01d1841c7c045e45dd311e8ea
LAPTOP-GLOUD29L\spoly <lorenzo.spoletini77@gmail.com>
2021-12-17 16:44:01 +0100

    inizio sviluppo backend/frontend. implementazione bootstrap e fontawesome

---
+++ Models/SpesaListViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace ProgettoSpesa.Models
+{
+    public class SpesaListViewModel
+    {
+        public List<SpesaListViewModel> SpesaLista { get; private set; }
+        public SpesaListViewModel()
+        {
+            this.SpesaLista = new List<SpesaListViewModel>();
+        }
+    }
+}
---
+++ Models/SpesaViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProgettoSpesa.Models
+{
+    public class SpesaViewModel
+    {
+     [Required]
+        public long id { get; set; }
+        public string prodotto { get; set; }
+        public int quantita { get; set; }
+        public decimal prezzo { get; set; }
+    }
+}
